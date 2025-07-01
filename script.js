aec6d3d038965da700ccf78c248b1e0641e57e15
sujal932538 <sujaltitare1gmail.com>
2025-07-01 22:05:04 +0530

    intial commit

--- script.js
+++ script.js
@@ -58,13 +58,15 @@ document.addEventListener("mousemove",function(dets){
 
 })
 
-// document.querySelectorAll("#child").addEventListener("mouseenter",function(){
-//     gsap.to("cursor",{
-//         transform: 'translate(-50%,-50%) scale(1)'
+ document.querySelectorAll("#child").addEventListener("mouseenter",function(){
+     gsap.to("cursor",{
+         transform: 'translate(-50%,-50%) scale(1)'
 
-//     })
-// })
+     })
+ })
 
-// document.querySelector("#child1").addEventListener("mouseenter",function(){
-//     gsap.to("cursor",{
-//         transform: 'translate(-50%,-50%) scale(0)'
+ document.querySelector("#child1").addEventListener("mouseenter",function(){
+     gsap.to("cursor",{
+         transform: 'translate(-50%,-50%) scale(0)'
+     })
+})
